b20293f7d59aaa2e0907bc367ad4586fc8b6f8c4
Flavio Scaccia <flavio.scaccia@gmail.com>
2024-06-23 22:09:45 +0200

    chore: cleaning home and first test

--- test/main.test.tsx
+++ test/main.test.tsx
@@ -1,5 +1,12 @@
-import { test, expect } from "vitest"
+import { describe, expect, it } from "vitest"
+import { render, screen } from '@testing-library/react'
 
-test("true should be true", () => {
-    expect(true).toBe(true)
+import Page from '../app/page'
+
+describe("Homepage", () => {
+    it("should render the component and show the home profile", () => {
+        render(<Page />)
+        const homescreenTitleId = screen.getByTestId("homescreen-title")
+        expect(homescreenTitleId).toBeDefined()
+    })
 })
