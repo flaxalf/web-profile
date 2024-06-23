6625110dcf7f2318a82bc2e1e89961770948690b
Flavio Scaccia <37453989+flaxalf@users.noreply.github.com>
2024-06-23 22:18:40 +0200

    Chore: configure vitest framework (#2)
    * chore: configuration and first simple test
    
    * chore: cleaning home and first test
    
    * chore: lint/format

---
+++ test/main.test.tsx
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import Page from "../app/page";
+
+describe("Homepage", () => {
+	it("should render the component and show the home profile", () => {
+		render(<Page />);
+		const homescreenTitleId = screen.getByTestId("homescreen-title");
+		expect(homescreenTitleId).toBeDefined();
+	});
+});
