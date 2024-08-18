be554a1169b6c72a1cdd396e3cd7fa8cd1e2f44b
Flavio Scaccia <flavio.scaccia@gmail.com>
2024-08-18 15:41:06 +0200

    fix: package dep 2

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Flavio Scaccia.
+      <time>{YEAR}</time> © Kathan Mehta.
       <style jsx>{`
         a {
           float: right;
