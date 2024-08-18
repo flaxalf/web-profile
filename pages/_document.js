ba07aa7f3e63b3b560702f9a6b08035306ec236f
Flavio Scaccia <flavio.scaccia@gmail.com>
2024-08-18 13:21:48 +0200

    feat: template creation

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  const meta = {
+    title: 'flaxalf',
+    description: 'my very simple personal portfolio',
+    image: './public/images/dog-sunglasses.jpg',
+  }
+
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:site_name" content={meta.title} />       
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
